db24ca548a5ac9edb345c21b0b6c56387878bc22
Anurag-dev-99 <64470246+Anurag-dev-99@users.noreply.github.com>
2026-09-18 23:49:50 +0530

    added aec -4 english pyq on website

---
+++ add_aec4_english_pyq.py
@@ -0,0 +1,24 @@
+﻿import json
+
+file_json = "C:\\Users\\Anurag\\Documents\\GitHub\\TataCollegeHub\\public\\data\\pyqs.json"
+with open(file_json, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+new_entry = {
+    "id": "pyq-aec-4-english-sem4-2022",
+    "title": "AEC-4 English 2022 Paper",
+    "year": "2022-2026",
+    "semester": 4,
+    "category": "AEC",
+    "subject": "English",
+    "downloadUrl": "https://drive.google.com/file/d/1xSCuprUiIQxIs1diDthM0Ubnv4B-f6Jt/view?usp=drive_link",
+    "downloadCount": 0,
+    "fileSize": "PDF File"
+}
+
+data.append(new_entry)
+
+with open(file_json, "w", encoding="utf-8") as f:
+    json.dump(data, f, indent=4, ensure_ascii=False)
+
+print(f"Added {new_entry['id']} to pyqs.json")
---
+++ check_pyq.py
@@ -0,0 +1,8 @@
+﻿import json
+
+file_json = "C:\\Users\\Anurag\\Documents\\GitHub\\TataCollegeHub\\public\\data\\pyqs.json"
+with open(file_json, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+print("Last entry in pyqs.json:")
+print(json.dumps(data[-1], indent=4))
